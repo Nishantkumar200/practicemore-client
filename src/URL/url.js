6b2dbcca353df133e3a822cc9704a64b04fbb7f3
Nishant kumar <nishantkumarsharma0@gmail.com>
2021-07-11 20:34:40 +0530

    update3

--- src/URL/url.js
+++ src/URL/url.js
@@ -1,2 +1,2 @@
-export const URL = "https://nishantsite.herokuapp.com";
+export const URL = "https://interviewpractise.herokuapp.com/";
 // export const URL = "https://practiceinterview.herokuapp.com/";
